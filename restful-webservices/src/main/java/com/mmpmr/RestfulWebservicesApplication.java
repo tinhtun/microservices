f203f38e144dd05415d15698918c3c2bc363d37d
tinhtun <tinhtun.86@gmail.com>
2018-02-10 00:45:39 +0800

    Added project restful-webservices

---
+++ restful-webservices/src/main/java/com/mmpmr/RestfulWebservicesApplication.java
@@ -0,0 +1,12 @@
+package com.mmpmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesApplication.class, args);
+	}
+}
