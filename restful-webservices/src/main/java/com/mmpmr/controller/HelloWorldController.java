6798532dfb9158f32a1aaac368fbd41488e6d36c
Tin Htun <tinhtun.86@gmail.com>
2018-02-13 22:04:28 +0800

    Add User

--- restful-webservices/src/main/java/com/mmpmr/controller/HelloWorldController.java
+++ restful-webservices/src/main/java/com/mmpmr/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.mmpmr.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello/{name}")
+	public String helloWithPathVar(@PathVariable String name) {
+		return String.format("Hello, %s", name);
 	}
 }
---
+++ restful-webservices/src/main/java/com/mmpmr/model/User.java
@@ -0,0 +1,38 @@
+package com.mmpmr.model;
+
+import java.util.Date;
+
+public class User {
+	private int id;
+	private String name;
+	private Date birthDate;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+}
